c3009a1fabb7ab7b6b0fc3a984de3ffae34dd2b0
avaashh <avaashbhattarai@gmail.com>
2025-12-04 02:37:59 -0600

    each execute function should use execute state

--- language/executor.c
+++ language/executor.c
@@ -58,15 +58,26 @@ void execute(Dataframe* df, Query* query, ExecutionState* state, time_t timeout)
         case AVERAGE:
             return execute_average(df, query->column_index, state, timeout);
         case MEDIAN:
-            return execute_median(df, query->column_index);
+            return execute_median(df, query->column_index, state, timeout);
         case INCREMENT:
-            return execute_increment(df, query->column_index, query->arg1);
+            return execute_increment(
+                df, query->column_index, query->arg1, state, timeout
+            );
         case WRITE:
-            return execute_write(df, query->column_index, query->arg1);
+            return execute_write(
+                df, query->column_index, query->arg1,
+                state, timeout
+            );
         case WRITE_AT:
-            return execute_write_at(df, query->column_index, (int)query->arg1, query->arg2);
+            return execute_write_at(
+                df, query->column_index, (int)query->arg1,
+                query->arg2, state, timeout
+            );
         case COUNT:
-            return execute_count(df, query->column_index, (char)((int)query->arg1), query->arg2);
+            return execute_count(
+                df, query->column_index, (char)((int)query->arg1),
+                query->arg2, state, timeout
+            );
         default:
             perror("Execute Error: Unknown operation");
             exit(1);
@@ -127,23 +138,40 @@ void execute_average(
     printf("AVERAGE(%d): %f\n", column_index, state->tally / (df->num_rows - 1));
 }
 
-void execute_median(Dataframe* df, int column_index) {
+void execute_median(
+    Dataframe* df, int column_index, ExecutionState* state, time_t timeout
+) {
     printf("Executing MEDIAN on column %d\n", column_index);
+    state->status = COMPLETED;
 }
 
-void execute_increment(Dataframe* df, int column_index, double value) {
+void execute_increment(
+    Dataframe* df, int column_index, double value,
+    ExecutionState* state, time_t timeout
+) {
     printf("Executing INCREMENT on column %d by %f\n", column_index, value);
+    state->status = COMPLETED;
 }
 
-void execute_write(Dataframe* df, int column_index, double value) {
+void execute_write(
+    Dataframe* df, int column_index, double value,
+    ExecutionState* state, time_t timeout
+) {
     printf("Executing WRITE on column %d with value %f\n", column_index, value);
+    state->status = COMPLETED;
 }
 
-void execute_write_at(Dataframe* df, int column_index, int row_index, double value) {
+void execute_write_at(
+    Dataframe* df, int column_index, int row_index, double value, ExecutionState* state, time_t timeout
+) {
     printf("Executing WRITE_AT on column %d at row %d with value %f\n", column_index, row_index, value);
+    state->status = COMPLETED;
 }
 
-void execute_count(Dataframe* df, int column_index, int comparison_operator, double value) {
+void execute_count(
+    Dataframe* df, int column_index, int comparison_operator, double value,
+    ExecutionState* state, time_t timeout
+) {
     if (column_index < 0 || column_index >= df->num_cols) {
         perror("COUNT Error: Column index out of range");
         exit(1);
