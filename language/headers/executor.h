901e77c86a6da0213006318e6ecd8ebd33a4da86
devanshu <devanshusenp@gmail.com>
2025-12-05 11:42:55 -0600

    exec: make format

--- language/headers/executor.h
+++ language/headers/executor.h
@@ -4,63 +4,63 @@
 #include "parser.h"
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 
 typedef struct {
-    // number of rows in the dataframe
-    int num_rows;
+	// number of rows in the dataframe
+	int num_rows;
 
-    // number of columns in the dataframe
-    int num_cols;
+	// number of columns in the dataframe
+	int num_cols;
 
-    // file descriptor for the opened CSV file
-    FILE* file;
+	// file descriptor for the opened CSV file
+	FILE *file;
 
-    // df enforces fixed-length cells
-    int cell_length;
+	// df enforces fixed-length cells
+	int cell_length;
 
-    // header row length in characters
-    int header_length;
+	// header row length in characters
+	int header_length;
 
-    // row width in characters (including commas and newline)
-    int row_width;
+	// row width in characters (including commas and newline)
+	int row_width;
 } Dataframe;
 
 typedef struct {
-    // user query is a part of execution state
-    Query* query;
+	// user query is a part of execution state
+	Query *query;
 
-    // to keep track of how much work was done previously
-    int processed_rows;
+	// to keep track of how much work was done previously
+	int processed_rows;
 
-    // intermediate result storage
-    double tally;
+	// intermediate result storage
+	double tally;
 
-    // execution status
-    enum { CREATED, INPROGRESS, COMPLETED } status;
+	// execution status
+	enum { CREATED, INPROGRESS, COMPLETED } status;
 
-    // position in file stream
-    long stream_position;
+	// position in file stream
+	long stream_position;
 } ExecutionState;
 
-void initialize(Dataframe* df, const char* file_path);
+void initialize(Dataframe *df, const char *file_path);
 
-void execute(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void execute_average(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute_average(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void execute_median(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute_median(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void execute_increment(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute_increment(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void execute_write(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute_write(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void execute_write_at(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute_write_at(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void execute_count(Dataframe* df, ExecutionState* state, time_t timeout);
+void execute_count(Dataframe *df, ExecutionState *state, time_t timeout);
 
-void cleanup(Dataframe* df);
+void cleanup(Dataframe *df);
 
 #endif
