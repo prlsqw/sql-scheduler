208af34425d4e7f38c4229ab8d63b895bcb2f441
avaashh <avaashbhattarai@gmail.com>
2025-12-07 15:50:17 -0600

    a queue to store jobs that arrive in the scheduler

---
+++ scheduler/headers/job_queue.h
@@ -0,0 +1,49 @@
+#include "../scheduler.h"
+
+#define INITIAL_JOB_QUEUE_CAPACITY 16
+
+typedef struct {
+	int size;
+	int capacity;
+	int iter;
+
+	Job **jobs;
+} JobQueue;
+
+/**
+ * Initialize the job queue
+ *
+ * \param queue  pointer to queue
+ */
+void initialize_job_queue(JobQueue *queue);
+
+/**
+ * Add a job to the queue
+ *
+ * \param queue  pointer to queue
+ * \param job  pointer to the job to be added
+ */
+void add_job_to_queue(JobQueue *queue, Job *job);
+
+/**
+ * Remove a job from the queue
+ *
+ * \param queue  pointer to queue
+ * \param job  pointer to the job to be removed
+ */
+void remove_job_from_queue(JobQueue *queue, Job *job);
+
+/**
+ * A JobQueue iterator
+ *
+ * \param queue  pointer to queue
+ * \return       pointer to the next job, or NULL if the queue is empty
+ */
+Job *next_job(JobQueue *queue);
+
+/**
+ * Free the memory allocated for the job queue
+ *
+ * \param queue  pointer to queue
+ */
+void cleanup_job_queue(JobQueue *queue);
