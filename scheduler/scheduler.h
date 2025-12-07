bb77b0e5878ba6b2575f15f28d67901510ec4156
avaashh <avaashbhattarai@gmail.com>
2025-12-07 15:52:13 -0600

    define a scheduler

---
+++ scheduler/scheduler.h
@@ -0,0 +1,41 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H 1
+
+#include <sys/time.h>
+#include "headers/job_queue.h"
+#include "../language/language.h"
+
+
+#define DEFAULT_QUANTUM 100
+
+typedef struct {
+    // queue of jobs to be scheduled
+    JobQueue queue;
+
+    // dataframe this scheduler is managing
+    Dataframe *df;
+    
+    // in ms
+    time_t quantum;
+
+    // max life; scheduler stops after this time if no new queries arrive
+    time_t max_life;
+    
+    // scheduling algorithm
+    void (*algorithm)(JobQueue *queue, time_t quantum, time_t max_life);
+} Scheduler;
+
+
+// A job is a DataFrame along with its query's execution state
+typedef struct {
+    Dataframe *df;
+    ExecutionState *state;
+} Job;
+
+
+// Round Robin, Weighted Round Robin, First In First Out
+typedef enum {
+    RR, WRR, FIFO
+} SchAlgorithm;
+
+#endif
