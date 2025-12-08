8d1b6652e7608c300f2964d1e685c7ecf27449d0
avaashh <avaashbhattarai@gmail.com>
2025-12-07 20:20:57 -0600

    expose language utils thru header

--- language/language.h
+++ language/language.h
@@ -1,8 +1,9 @@
 #ifndef PQL_LANG_H
 #define PQL_LANG_H 1
 
+#include "headers/executor.h"
 #include "headers/grammar.h"
 #include "headers/parser.h"
-#include "headers/executor.h"
+#include "headers/utils.h"
 
 #endif
