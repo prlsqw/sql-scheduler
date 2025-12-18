156b787e626be78cce07359f5d56250330b49e87
devanshu <devanshusenp@gmail.com>
2025-12-17 19:56:19 -0600

    weights bug fix

--- scheduler/weights.c
+++ scheduler/weights.c
@@ -7,6 +7,9 @@ double get_operation_quantum(Dataframe *df, Query *query,
 	double time_tally = 0;
 	int count_tally = 0;
 
+	// rewind the file to start reading from the beginning
+	rewind(df->weights);
+
 	// read all weights to calculate total time
 	int N = ARRAY_LEN(QueryOps);
 	for (int i = 0; i < N; i++) {
@@ -26,6 +29,10 @@ double get_operation_quantum(Dataframe *df, Query *query,
 	// taken by this operation compared to others
 	double this_op_avg = count_tally ? time_tally / count_tally : 0;
 	double all_ops_avg = total_time / N;
+
+	if (all_ops_avg == 0) {
+		return baseline_quantum;
+	}
 
 	double weight = (this_op_avg + all_ops_avg) / all_ops_avg;
 
@@ -54,6 +61,6 @@ void update_operation_weight(Dataframe *df, Query *query,
 	fseek(df->weights, -1 * (WEIGHT_TIME_WIDTH + WEIGHT_COUNT_WIDTH + 2),
 		  SEEK_CUR);
 
-	fprintf(df->weights, "%*lf %*d\n", WEIGHT_TIME_WIDTH, current_time,
+	fprintf(df->weights, "%-*lf %-*d\n", WEIGHT_TIME_WIDTH, current_time,
 			WEIGHT_COUNT_WIDTH, current_count);
 }
